261934d02bf0a61a4b91d82e4efd8fa68b9b2eb6
mdiaz26 <mike.diaz006@gmail.com>
2020-07-13 10:39:22 -0400

    I changed the opaqueness argument in background-image so that the bottom of the skyline is darker, to make the text pop

--- assets/contactform.php
+++ assets/contactform.php
@@ -29,3 +29,50 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']))
     }
 }
 ?>
+
+<!doctype html>
+<html>
+    <article id="contact">
+        <h2 class="major">Contact Us</h2>
+        <form method="post" action=”contactform.php”>
+            <div class="fields">
+                <div class="field half">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" />
+                </div>
+                <div class="field half">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" />
+                </div>
+                <div class="field">
+                    <label for="message">Message</label>
+                    <textarea name="message" id="message" rows="4"></textarea>
+                </div>
+            </div>
+            <ul class="actions">
+                <li><input type="submit" value="Send Message" class="primary" /></li>
+                <li><input type="reset" value="Reset" /></li>
+            </ul>
+        </form>
+        <ul class="icons">
+            <li><a href="https://www.linkedin.com/in/jeanlucboulnois/" target="_blank" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+            <!-- <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li> -->
+        </ul>
+
+        <span class="tile-list">
+            <nav>
+                <ul>
+                    <li><a href="#mission">Mission</a></li>
+                    <li><a href="#about-us">About Us</a></li>
+                    <li><a href="#team">Team</a></li>
+                    <li><a href="#case-studies">Case Studies</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <!--<li><a href="#elements">Elements</a></li>-->
+                </ul>
+            </nav>
+        </span>
+    </article>
+</html>
